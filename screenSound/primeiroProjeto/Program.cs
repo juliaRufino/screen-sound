a0ec85761cb30e50bcf8f7a432d6376a569bd90a
Julia Rufino <juliarufino@MacBook-Pro-de-Henrique.local>
2023-09-04 14:51:03 -0300

    Adicionando as funções de avaliar uma banda e exibir a média de suas notas

--- screenSound/primeiroProjeto/Program.cs
+++ screenSound/primeiroProjeto/Program.cs
@@ -1,6 +1,9 @@
 ﻿//Screen Sound
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound!";
-List<String> listaDasBandas = new List<string>();
+//List<String> listaDasBandas = new List<string>();
+
+//Criando um dicionario chave = string valores = list<int>
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
 
 void ExibirLogo()
 {
@@ -41,10 +44,10 @@ void ExibirOpcoesDoMenu()
             MostrarBandasRegistradas();
             break;
         case 3:
-            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            AvaliarUmaBanda();
             break;
         case 4:
-            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            ExibirMediaDeUmaBanda(); ;
             break;
         case -1:
             Console.WriteLine("Tchau tchau! :)");
@@ -55,15 +58,15 @@ void ExibirOpcoesDoMenu()
     }
 }
 
+ExibirLogo();
+ExibirOpcoesDoMenu();
+
 void RegistrarBanda()
 {
     //Limpar console
     Console.Clear();
 
-    Console.WriteLine("**************************");
-    // Exibe a mensagem no console mas pula uma linha
-    Console.WriteLine("Registro de bandas");
-    Console.WriteLine("**************************");
+    ExibirTituloDaOpcao("Registro de Bandas");
 
     // Exibe o a mensagem no console mas recebe o valor do usuário na mesma linha
     Console.Write("Digite o nome da banda que deseja registrar: ");
@@ -71,7 +74,8 @@ void RegistrarBanda()
     // A exclamação serve para indicar que nao aceitamos numero nulos
     string nomeDaBanda = Console.ReadLine()!;
 
-    listaDasBandas.Add(nomeDaBanda);
+    //Adicionando os valores no dicionario, e nao temos nota ainda para adicionar
+    bandasRegistradas.Add(nomeDaBanda, new List<int>());
 
     Console.WriteLine("Registrando a banda....");
 
@@ -80,7 +84,7 @@ void RegistrarBanda()
 
     Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
 
-    Thread.Sleep(2000);
+    Thread.Sleep(4000);
 
     //Limpa o console
     Console.Clear();
@@ -89,20 +93,105 @@ void RegistrarBanda()
     ExibirOpcoesDoMenu();
 }
 
-ExibirLogo();
-ExibirOpcoesDoMenu();
-
 void MostrarBandasRegistradas()
 {
     Console.Clear();
 
-    Console.WriteLine("**************************");
-    Console.WriteLine("Registro de bandas");
-    Console.WriteLine("**************************");
+    ExibirTituloDaOpcao("Bandas registradas");
 
-    for (int i = 0; i < listaDasBandas.Count(); i++)
+    //for (int i = 0; i < listaDasBandas.Count(); i++)
+    //{
+    //    Console.WriteLine("\nBanda:" + listaDasBandas[i]);
+    //}
+
+    //Caminha estre todos os elementos, e pra cada elemento realiza algo
+    foreach (string banda in bandasRegistradas.Keys)
     {
-        Console.WriteLine("\n" + listaDasBandas[i]);
+        Console.WriteLine($"Banda: {banda}");
+    }
+
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirLogo();
+    ExibirOpcoesDoMenu();
+}
+
+void ExibirTituloDaOpcao(string titulo)
+{
+    int qtdDeLetras = titulo.Length;
+
+    // Adiciona elementos a esquerda, recebe 2 argumentos, a quantidade e qual elemento
+    string asteriscos = string.Empty.PadLeft(qtdDeLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos + "\n");
+}
+
+void AvaliarUmaBanda()
+{
+    Console.Clear();
+    //digite qual banda deseja avaliar
+    //se a banda existir no dicionario => atribuir uma nota
+    //senão, voltar ao menu
+
+    ExibirTituloDaOpcao("Avaliar Banda");
+    Console.Write("Digite o nome da banda que deseja avaliar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+
+    //Verificacão se aquela banda foi registrada
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
+
+        //Pegando a nota que o usario digtou, e o parse é para converter a string para int
+        //O exclamação é porque precisa ser uma string nao queremos outro valor/nem ser vazio
+        int nota = int.Parse(Console.ReadLine()!);
+        bandasRegistradas[nomeDaBanda].Add(nota);
+        Console.WriteLine($"\nA nota {nota} foi registrada com sucesso para a banda {nomeDaBanda}");
+        Thread.Sleep(2000);
+        Console.Clear();
+        ExibirLogo();
+        ExibirOpcoesDoMenu();
+
+    }else
+    {
+        Console.WriteLine($"A banda {nomeDaBanda} não foi encontrada!");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirLogo();
+        ExibirOpcoesDoMenu();
+    }
+
+}
+
+void ExibirMediaDeUmaBanda()
+{
+    Console.Clear();
+
+    ExibirTituloDaOpcao("Exibir média das notas");
+    Console.WriteLine("Digite o nome da banda que deseja visualizar a média das notas: ");
+    string nomeDaBanda = Console.ReadLine()!;
+
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        List<int> notasDasBandas = bandasRegistradas[nomeDaBanda];
+        Console.WriteLine($"A média da banda {nomeDaBanda} é {notasDasBandas.Average()}.");
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirLogo();
+        ExibirOpcoesDoMenu();
+
+    } else
+    {
+        Console.WriteLine($"A banda {nomeDaBanda} não foi encontrada!");
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirLogo();
+        ExibirOpcoesDoMenu();
     }
 
 }
